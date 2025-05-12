7a6f65871080232187db92e402c92b5c58df4209
Carlos Rodrigues <c.rodrigues.edu@gmail.com>
2025-05-12 00:31:37 -0300

    Att. v.2 add. curriculo

---
+++ js/curriculo.js
@@ -0,0 +1,11 @@
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add('show');
+    }
+  });
+}, {
+  threshold: 0.15,
+});
+
+document.querySelectorAll('.card').forEach(el => observer.observe(el));
